671309cac0b4207a6ec95ffb2f2430d12e6c5537
ruriknj <ruriknj@hotmail.com>
2021-01-05 20:37:46 -0300

    GET /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/services/ProductService.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/ProductService.java
@@ -20,7 +20,7 @@ public class ProductService {
 	@Transactional(readOnly = true)
 	public List<ProductDTO> findAll() {
 		
-		List<Product> list = repository.findAllByOrderByNameAsc();
+		List<Product> list = repository.findAll();
 		
 		return list.stream().map(x -> new ProductDTO(x)).collect(Collectors.toList());
 	}
